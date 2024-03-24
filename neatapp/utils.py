854b6fa2520f41970330fba0dc5647789a3ded13
Chaipat Suwannapoom <bchaipats@gmail.com>
2024-03-24 15:47:34 +0700

    Add docstring to functions

--- neatapp/utils.py
+++ neatapp/utils.py
@@ -6,7 +6,20 @@ from llama_index.multi_modal_llms.openai import OpenAIMultiModal
 from neatapp.constants import STORAGE_DIR
 
 
-def get_multi_modal_llm(llm_name, model_temperature, openai_api_key, max_new_tokens=1000):
+def get_multi_modal_llm(
+    llm_name: str, model_temperature: int, openai_api_key: str, max_new_tokens: int = 1000
+) -> OpenAIMultiModal:
+    """Create a multimodal LLM from OpenAI
+
+    Args:
+        llm_name (str): name of the OpenAI GPT model
+        model_temperature (int): model temperature ranging from 0-1
+        openai_api_key (str): an OPENAI_API_KEY used to authenticate with the OpenAI platform
+        max_new_tokens (int, optional): number of maximum tokens generated. Defaults to 1000.
+
+    Returns:
+        OpenAIMultiModal: _description_
+    """
     llm = OpenAIMultiModal(
         model=llm_name,
         temperature=model_temperature,
